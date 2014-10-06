536ca31555ba3af95db0753a0964089b9d43abb8
eshklyar <eshklyar@gmail.com>
2014-10-06 13:38:12 -0500

    Initial Commit

---
+++ Deprocrastinator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Deprocrastinator
+//
+//  Created by Edik Shklyar on 10/6/14.
+//  Copyright (c) 2014 Edik Shklyar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
